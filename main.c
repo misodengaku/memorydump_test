83a55b0e3b45dbd5b830ccc74f8b5108cb49470a
misodengaku <misodengaku@gmail.com>
2014-09-18 01:44:44 +0900

    fix

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	
 	if (argc != 4)
 	{
-		printf("Usage: %s pid startaddr endaddr\n", argv[0], argv[0]);
+		printf("Usage: %s pid startaddr endaddr\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	else
